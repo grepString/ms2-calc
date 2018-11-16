5a0047cff0bf43da8543c32141e1b68374ad9467
grepString <gagetaylor@pm.me>
2018-11-16 14:56:27 -0500

    added roi percentage

--- js/calcR.js
+++ js/calcR.js
@@ -7,8 +7,16 @@ function ms2CalcR()
         fc2 = rp * q;
         mp = fc2 - (fc2 * .1);
         roi = mp - fc;
+        incG = (roi / fc) * 100;
         document.getElementById("finalCost").innerHTML = fc;
         //document.getElementById("finalCost2").innerHTML = fc2;
         document.getElementById("marketProfit").innerHTML = mp;
         document.getElementById("roi").innerHTML = roi;
+        document.getElementById("inc").innerHTML = incG.toFixed(2) + '%';
+
+        var elt = document.getElementById("gains");
+        if (roi >= 1)
+            elt.style.cssText = 'color: #2ecc71;';
+        else if (roi <= 0)  
+            elt.style.cssText = 'color: #e74c3c;';
 }
