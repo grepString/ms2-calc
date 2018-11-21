69d710cfb476c8dad073c461650a9d6bd38ee550
grepString <gagetaylor@pm.me>
2018-11-20 20:41:42 -0500

    removed old file

--- js/calc.js
+++
@@ -1,9 +0,0 @@
-function ms2Calc()
-{
-        ppi = document.getElementById("inputPPI").value;
-        q1 = document.getElementById("inputQuantity").value;
-        fc1 = ppi * q1;
-        p1 = fc1 - (fc1 * .1);
-        document.getElementById("cost").innerHTML = fc1;
-        document.getElementById("profit").innerHTML = p1;
-}
--- js/calcR.js
+++ js/calcR.js
@@ -1,22 +1,21 @@
-function ms2CalcR()
-{
-        bp = document.getElementById("inputBP").value;
-        q = document.getElementById("inputQuantity").value;
-        fc = bp * q;
-        rp = document.getElementById("inputRP").value;
-        fc2 = rp * q;
-        mp = fc2 - (fc2 * .1);
-        roi = mp - fc;
-        incG = (roi / fc) * 100;
-        document.getElementById("finalCost").innerHTML = fc;
-        //document.getElementById("finalCost2").innerHTML = fc2;
-        document.getElementById("marketProfit").innerHTML = mp;
-        document.getElementById("roi").innerHTML = roi;
-        document.getElementById("inc").innerHTML = incG.toFixed(2) + '%';
+function ms2CalcR() {
+    bp = document.getElementById("inputBP").value;
+    q = document.getElementById("inputQuantity").value;
+    fc = bp * q;
+    rp = document.getElementById("inputRP").value;
+    fc2 = rp * q;
+    mp = fc2 - (fc2 * .1);
+    roi = mp - fc;
+    incG = (roi / fc) * 100;
+    document.getElementById("finalCost").innerHTML = fc;
+    //document.getElementById("finalCost2").innerHTML = fc2;
+    document.getElementById("marketProfit").innerHTML = mp;
+    document.getElementById("roi").innerHTML = roi;
+    document.getElementById("inc").innerHTML = incG.toFixed(2) + '%';
 
-        var elt = document.getElementById("gains");
-        if (roi >= 1)
-            elt.style.cssText = 'color: #2ecc71;';
-        else if (roi <= 0)  
-            elt.style.cssText = 'color: #e74c3c;';
+    var elt = document.getElementById("gains");
+    if (roi >= 1)
+        elt.style.cssText = 'color: #2ecc71;';
+    else if (roi <= 0)
+        elt.style.cssText = 'color: #e74c3c;';
 }
